abb7d15681d42336f9c102f9a17229d5e9c7e5a8
학생 이하영 <sjo6382@knou.ac.kr>
2026-01-23 15:39:35 +0900

    리팩토링 - CommentsRepository 각 역할을 주석으로 정리

--- src/main/java/com/example/TravelProject/comment/CommentsRepository.java
+++ src/main/java/com/example/TravelProject/comment/CommentsRepository.java
@@ -7,14 +7,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CommentsRepository extends JpaRepository<Comments, Long> {
-	
+
+    // 게시글 유효 번호를 통해 해당 게시글의 전체 댓글 선택
 	@Query(value="SELECT * FROM comments WHERE post_id = :post_id ORDER BY comment_gup, comment_seq",
 			nativeQuery = true)
 	List<Comments> findCommentsByPostId(Long post_id);
-	
+
+    // 전체 댓글 수
 	@Query(value="SELECT COUNT(*) FROM comments WHERE post_id = :post_id", nativeQuery = true)
 	int selectAllComments(Long post_id);
 
+    // 블로그 유효 번호를 통해 최신 댓글 선택
 	@Query(value="SELECT * FROM comments WHERE blog_id = :blog_id AND comment_del IS NULL ORDER BY comment_id DESC Limit 0, 5",
 			nativeQuery = true)
 	List<Comments> findCommentsByBlogId(Long blog_id);
