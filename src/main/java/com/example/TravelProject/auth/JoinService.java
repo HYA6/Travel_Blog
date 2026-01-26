2ae5c5da9797f40a884e89912de9252ec8ee2fe4
학생 이하영 <sjo6382@knou.ac.kr>
2026-01-26 15:40:54 +0900

    리팩토링 - 회원가입 시 DB 형식에 맞게 변환하는 로직을 controller에서 service로 분리

--- src/main/java/com/example/TravelProject/auth/JoinService.java
+++ src/main/java/com/example/TravelProject/auth/JoinService.java
@@ -1,4 +1,34 @@
 package com.example.TravelProject.auth;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+
+@Slf4j
+@Service
 public class JoinService {
+
+    // 로그인 경로 확인, DB 형식에 맞게 변환
+    public UsersDto joinChk(HttpSession session, UsersDto usersDto) {
+        log.info("JoinService의 loginChk() 메소드");
+        Date nowDate = new Date();
+
+        if (usersDto.getUserId() == null) {
+            usersDto = (UsersDto) session.getAttribute("usersDto"); // form 사용X (로그인 API 이용)
+        } else {
+            // form으로 넘어온 생일 데이터로 나이 저장 (회원가입 이용)
+            int birthYear = Integer.parseInt(usersDto.getUserBirhtday().substring(0,4));
+            @SuppressWarnings("deprecation")
+            int year = nowDate.getYear() + 1900;
+            usersDto.setUserAge(year - birthYear);
+        }
+
+        // 생성일에 오늘 날짜 넣어주기(yyyy-MM-dd)
+        usersDto.setUserCreateDate(nowDate);
+
+        return usersDto;
+    };
 }
