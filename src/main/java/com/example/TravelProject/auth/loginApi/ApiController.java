0925b3189f1827a22a366d7b33330824049964ae
학생 이하영 <sjo6382@knou.ac.kr>
2026-02-03 15:53:36 +0900

    naver login 임시 주석

--- src/main/java/com/example/TravelProject/auth/loginApi/ApiController.java
+++ src/main/java/com/example/TravelProject/auth/loginApi/ApiController.java
@@ -32,35 +32,35 @@ public class ApiController {
 	public String callback(HttpSession session, @RequestParam String code, 
 			@RequestParam String state) throws IOException, ParseException {
 		
-		OAuth2AccessToken oauthToken = naverLoginBO.getAccessToken(session, code, state);
-		
-		// 로그인 사용자 정보를 얻어온다.
-        String apiResult = naverLoginBO.getUserProfile(oauthToken);
-		
-		// String 형식인 로그인 사용자 정보를 json 형태로 바꾼다.
-        // ✅ Jackson 파싱
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode rootNode = objectMapper.readTree(apiResult);
-        JsonNode responseNode = rootNode.get("response");
-
-        String id = responseNode.get("id").asText();
-        String name = responseNode.get("name").asText();
-        String email = responseNode.get("email").asText();
-
-        String birthYear = responseNode.get("birthyear").asText();
-        String birthDay = responseNode.get("birthday").asText(); // MM-DD
-
-        LocalDate birthDate = LocalDate.parse(birthYear + "-" + birthDay);
-        int age = Period.between(birthDate, LocalDate.now()).getYears();
-		
-		UsersDto usersDto = new UsersDto();
-		usersDto.setUserId(id);
-		usersDto.setUserName(name);
-		usersDto.setUserEmail(email);
-		usersDto.setUserBirthday(birthDate);
-		
-		// 파싱된 값을 세션에 저장한다.
-		session.setAttribute("usersDto", usersDto);
+//		OAuth2AccessToken oauthToken = naverLoginBO.getAccessToken(session, code, state);
+//
+//		// 로그인 사용자 정보를 얻어온다.
+//        String apiResult = naverLoginBO.getUserProfile(oauthToken);
+//
+//		// String 형식인 로그인 사용자 정보를 json 형태로 바꾼다.
+//        // ✅ Jackson 파싱
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        JsonNode rootNode = objectMapper.readTree(apiResult);
+//        JsonNode responseNode = rootNode.get("response");
+//
+//        String id = responseNode.get("id").asText();
+//        String name = responseNode.get("name").asText();
+//        String email = responseNode.get("email").asText();
+//
+//        String birthYear = responseNode.get("birthyear").asText();
+//        String birthDay = responseNode.get("birthday").asText(); // MM-DD
+//
+//        LocalDate birthDate = LocalDate.parse(birthYear + "-" + birthDay);
+//        int age = Period.between(birthDate, LocalDate.now()).getYears();
+//
+//		UsersDto usersDto = new UsersDto();
+//		usersDto.setUserId(id);
+//		usersDto.setUserName(name);
+//		usersDto.setUserEmail(email);
+//		usersDto.setUserBirthday(birthDate);
+//
+//		// 파싱된 값을 세션에 저장한다.
+//		session.setAttribute("usersDto", usersDto);
 		
 		return "redirect:usersInsert";
 	}
